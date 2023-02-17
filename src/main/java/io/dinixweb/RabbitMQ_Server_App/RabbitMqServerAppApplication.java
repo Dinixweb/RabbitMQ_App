1796bbf6144b29d1252820147f8162fbcf02b27f
Dennis <sd4live@gmail.com>
2023-02-17 22:21:49 +0100

    initial commit

---
+++ src/main/java/io/dinixweb/RabbitMQ_Server_App/RabbitMqServerAppApplication.java
@@ -0,0 +1,13 @@
+package io.dinixweb.RabbitMQ_Server_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqServerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/dinixweb/RabbitMQ_Server_App/RabbitMqServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dinixweb.RabbitMQ_Server_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
