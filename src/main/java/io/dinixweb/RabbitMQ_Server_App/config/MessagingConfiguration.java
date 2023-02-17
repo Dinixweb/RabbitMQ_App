33ecebd26649c2765dc1bbf0a07725f324fd11af
Dennis <sd4live@gmail.com>
2023-02-17 22:59:10 +0100

    rabbitmq template

--- src/main/java/io/dinixweb/RabbitMQ_Server_App/config/MessagingConfiguration.java
+++ src/main/java/io/dinixweb/RabbitMQ_Server_App/config/MessagingConfiguration.java
@@ -1,9 +1,10 @@
 package io.dinixweb.RabbitMQ_Server_App.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -14,15 +15,28 @@ public class MessagingConfiguration {
     public static final String ROUTINGKEY = "dinix_routingKey";
 
 
+    @Bean
     public Queue queue(){
         return new Queue(QUEUE);
     }
 
+    @Bean
     public TopicExchange exchange(){
         return new TopicExchange(EXCHANGE);
     }
 
+    @Bean
     public Binding binding(Queue queue, TopicExchange exchange){
         return BindingBuilder.bind(queue).to(exchange).with(ROUTINGKEY);
     }
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+     @Bean
+    public AmqpTemplate template(ConnectionFactory connectionFactory){
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+     }
 }
